6a726ec1ff6073f8a003c38efa1630cc8d93b10b
Edip <edip.erturk@ogr.sakarya.edu.tr>
2018-05-24 17:26:09 +0300

    response fix.

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ mongoose.Promise = global.Promise;
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 require('./server/routes/word.routes.js')(app);
 
 mongoose.connect(dbConfig.url)
