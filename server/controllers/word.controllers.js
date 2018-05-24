d6e9acb8bab23bf455545d82d1b25c67dd1d57c1
Edip <edip.erturk@ogr.sakarya.edu.tr>
2018-05-24 17:42:21 +0300

    Controller updated.

--- server/controllers/word.controllers.js
+++ server/controllers/word.controllers.js
@@ -39,15 +39,78 @@ exports.findAll = (req, res) => {
 
 // Find a single word with a wordId
 exports.findOne = (req, res) => {
-
+    Word.findById(req.params.wordId)
+        .then(word => {
+            if (!word) {
+                return res.status(404).send({
+                    message: "Word not found with id " + req.params.wordId
+                });
+            }
+            res.send(word);
+        }).catch(err => {
+            if (err.kind === 'ObjectId') {
+                return res.status(404).send({
+                    message: "Word not found with id " + req.params.wordId
+                });
+            }
+            return res.status(500).send({
+                message: "Error retrieving word with id " + req.params.wordId
+            });
+        });
 };
 
 // Update a word identified by the wordId in the request
 exports.update = (req, res) => {
+    if (!req.body) {
+        return res.status(400).send({
+            message: "Word body can not be empty"
+        });
+    }
 
+    // Find word and update it with the request body
+    Word.findByIdAndUpdate(req.params.wordId, {
+        source: req.body.source,
+        target: req.body.target,
+        origin: req.body.origin,
+        translation: req.body.translation
+    }, { new: true })
+        .then(word => {
+            if (!word) {
+                return res.status(404).send({
+                    message: "Word not found with id " + req.params.wordId
+                });
+            }
+            res.send(word);
+        }).catch(err => {
+            if (err.kind === 'ObjectId') {
+                return res.status(404).send({
+                    message: "Word not found with id " + req.params.wordId
+                });
+            }
+            return res.status(500).send({
+                message: "Error updating word with id " + req.params.wordId
+            });
+        });
 };
 
 // Delete a word with the specified wordId in the request
 exports.delete = (req, res) => {
-
+    Word.findByIdAndRemove(req.params.wordId)
+    .then(word => {
+        if(!word) {
+            return res.status(404).send({
+                message: "Word not found with id " + req.params.wordId
+            });
+        }
+        res.send({message: "Word deleted successfully!"});
+    }).catch(err => {
+        if(err.kind === 'ObjectId' || err.name === 'NotFound') {
+            return res.status(404).send({
+                message: "Word not found with id " + req.params.wordId
+            });                
+        }
+        return res.status(500).send({
+            message: "Could not delete word with id " + req.params.wordId
+        });
+    });
 };
